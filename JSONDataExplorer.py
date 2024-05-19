07ea5fe1765a6b19e24e1e228b6d12ff11e5653b
kirthika85 <kirthika.rajamani@gmail.com>
2024-05-19 00:19:40 -0700

    Update JSONDataExplorer.py

--- JSONDataExplorer.py
+++ JSONDataExplorer.py
@@ -86,7 +86,7 @@ if openai_api_key.startswith('sk-'):
            prompt=ChatPromptTemplate.from_template(f"Query the following JSON data with: '{query_input}'\n\n{json_data}")
            chain=prompt|llm
            st.write(json_input)
-           response =response=chain.invoke({"json_data":{json_input}})
+           response =response=chain.invoke({"json_data":json_input})
            st.write("### Query Result")
            st.write(response.content)
       else:
