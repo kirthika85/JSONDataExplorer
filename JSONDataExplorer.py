46747a46a5f2dec86b874d3ebdceb8295184e071
kirthika85 <kirthika.rajamani@gmail.com>
2024-05-19 00:42:31 -0700

    Update JSONDataExplorer.py

--- JSONDataExplorer.py
+++ JSONDataExplorer.py
@@ -84,7 +84,7 @@ if openai_api_key.startswith('sk-'):
    if st.button("Query JSON"):
       if query_input and json_input :
            llm=ChatOpenAI(api_key=openai_api_key,temperature=0.1,model_name="gpt-3.5-turbo")
-           prompt=ChatPromptTemplate.from_template(f"'{query_input} in '\n\n{json_data}")
+           prompt=ChatPromptTemplate.from_template(f"'{query_input} in {json_data}")
            chain=prompt|llm
            st.write(json_input)
            st.write(query_input)
