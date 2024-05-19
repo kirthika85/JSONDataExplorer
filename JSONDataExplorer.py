15f508e119bcb23cb37417ed613c2df8513c4410
kirthika85 <kirthika.rajamani@gmail.com>
2024-05-19 00:44:21 -0700

    Update JSONDataExplorer.py

--- JSONDataExplorer.py
+++ JSONDataExplorer.py
@@ -90,6 +90,7 @@ if openai_api_key.startswith('sk-'):
            st.write(query_input)
            st.write(prompt)
            response=chain.invoke({"json_data": json_input})
+           st.write(prompt)
            st.write("### Query Result")
            st.write(response.content)
       else:
